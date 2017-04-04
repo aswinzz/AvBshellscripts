b304d8148bf1d2e3202948977bf9dace5ada0742
AVB <iit2016106@iiita.ac.in>
2017-04-04 18:35:29 +0530

    added options

--- avb.sh
+++ avb.sh
@@ -5,26 +5,26 @@ echo -e "
 Helps You Solve Issues You Face While Using Linux. 
 
  \e[4mSelect any options yu need : \e[0m
-\e[1m wifi \e[0m    : \e[2m Improve Signal Strength Of Adapter In Linux \e[0m
-\e[1m gitSetup \e[0m  : \e[2m Setup Git In Your Terminal \e[0m
-\e[1m uninstall \e[0m   : \e[2m Uninstall Any Software Easily \e[0m
-\e[1m gitaddproject \e[0m  : \e[2m Push A Complete Project Into Github\e[0m
-\e[1m c \e[0m  : \e[2m Do Coding With Less Commands \e[0m
+\e[1m 1-wifi \e[0m    : \e[2m Improve Signal Strength Of Adapter In Linux \e[0m
+\e[1m 2-gitSetup \e[0m  : \e[2m Setup Git In Your Terminal \e[0m
+\e[1m 3-uninstall \e[0m   : \e[2m Uninstall Any Software Easily \e[0m
+\e[1m 4-gitaddproject \e[0m  : \e[2m Push A Complete Project Into Github\e[0m
+\e[1m 5-c \e[0m  : \e[2m Do Coding With Less Commands \e[0m
 "
 read -p " Enter your choice : " choice
-if [ "$choice" = "wifi" ]; then
+if [ "$choice" = "1" ]; then
    sh wifi.sh
 fi
-if [ "$choice" = "wifi" ]; then
+if [ "$choice" = "2" ]; then
    sh gitsetup.sh
 fi
-if [ "$choice" = "uninstall" ]; then
+if [ "$choice" = "3" ]; then
    sh unisntall.sh
 fi
-if [ "$choice" = "gitaddproject" ]; then
+if [ "$choice" = "4" ]; then
    sh addproj.sh
 fi
-if [ "$choice" = "c" ]; then
+if [ "$choice" = "5" ]; then
    sh c.sh
 fi
 
